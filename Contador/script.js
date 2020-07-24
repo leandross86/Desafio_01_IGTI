d3082fcfa15882267f0b250fc27eefa9b82cf03c
Leandross86 <leandrobko@gmail.com>
2020-07-24 16:24:43 -0300

    setTimeout, setInterval e clearInterval

---
+++ Contador/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function () {
+    const timer = document.querySelector('#timer');
+    let count = 0;
+    const interval = setInterval(() => {
+        timer.textContent = ++count;
+
+        if (count ===20) {
+            this.clearInterval(interval);
+        }
+        if(count % 5 === 0) {
+            setTimeout(() => {
+                timer.textContent = count + ',5'
+            }, 500);
+        }
+    }, 1000);
+});
